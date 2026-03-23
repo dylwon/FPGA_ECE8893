9a61ab5dc7ea980cc613e72cba3e2ec80b80650f
dylwon <122586486+dylwon@users.noreply.github.com>
2026-03-23 18:40:04 -0400

    Add header file dcl.h for vision pipeline

---
+++ 2026_Spring/lab4/dcl.h
@@ -0,0 +1,18 @@
+#ifndef DCL_H
+#define DCL_H
+
+#include <ap_fixed.h>
+
+// Image dimensions (Can scale down to 128x128 for faster C-simulation during testing)
+#define HEIGHT 512
+#define WIDTH 512
+
+// Fixed-point data types satisfying the rubric requirement
+typedef ap_fixed<16, 8, AP_TRN, AP_WRAP> pixel_t;
+typedef ap_fixed<32, 16, AP_TRN, AP_WRAP> calc_t;
+typedef ap_fixed<16, 2, AP_TRN, AP_WRAP> coef_t;
+
+// Prototype for your hardware design
+void top_vision_pipeline(pixel_t img_in[HEIGHT][WIDTH], pixel_t img_out[HEIGHT][WIDTH]);
+
+#endif
